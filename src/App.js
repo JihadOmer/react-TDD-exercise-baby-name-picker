c3586810041c66ef7992290eb150e72f28225b26
Musa <79163385+musayuksel@users.noreply.github.com>
2022-12-28 20:46:59 +0000

    Complete header tests and component
    Co-authored-by: Jihad Omer <JihadOmer@users.noreply.github.com>
    Co-authored-by: Zaw Khing <zkhing@users.noreply.github.com>
    Co-authored-by: Shimaadnan <Shimaadnan@users.noreply.github.com>
    Co-authored-by: leilafaez <leilafaez@users.noreply.github.com>
    Co-authored-by: baharbazargan62 <baharbazargan62@users.noreply.github.com>
    Co-authored-by: Adibab <adiba.du.bd@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import "./App.css";
-
+import logo from "./assets/baby-carriage-solid.svg";
 function App() {
  
 
   return (
     <>
-    HELLO WORLD
+    <header data-testid='header'>
+      <img className='logo' src={logo} alt="baby-carriage-solid" />
+      <h3 data-testid='company-name'>Our Future...</h3>
+    </header>
     </>
   );
 }
 
 export default App;
+  
--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,28 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 describe('App', () => {
-it('tests',()=>{
-  expect(true).toBe(true);
-})
+  describe('header', () => {
+    it('should render the header', () => {
+      render(<App />);
+      expect(screen.getByTestId('header')).toBeVisible();
+    });
+    it('should be semantic HTML', () => {
+      render(<App />);
+      expect(screen.getByTestId('header').tagName).toBe('HEADER');
+    });
+    it('should show the logo', () => {
+      render(<App />)
+      expect(screen.getByAltText('baby-carriage-solid')).toBeVisible();
+    });
+    it('should add the style', () => {
+      render(<App />)
+      expect(screen.getByAltText('baby-carriage-solid').className).toBe('logo');
+    });
+    it('should add website name', () => {
+      render(<App />)
+      expect(screen.getByTestId('company-name')).toBeVisible()
+      expect(screen.getByTestId('company-name').tagName).toBe('H3')
+      expect(screen.getByTestId('company-name').textContent).toBe('Our Future...')
+    });
+  })
 });
